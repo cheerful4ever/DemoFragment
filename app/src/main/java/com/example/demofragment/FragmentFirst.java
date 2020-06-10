04286ff3de5977e27915fa82b0d35ba87d46c9d2
18016375 <cecilialui16@yahoo.com.sg>
2020-06-10 15:07:03 +0800

    activities 1-100000

--- app/src/main/java/com/example/demofragment/FragmentFirst.java
+++ app/src/main/java/com/example/demofragment/FragmentFirst.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentFirst extends Fragment {
+
+    Button btnAddText;
+    TextView tvFrag1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +63,21 @@ public class FragmentFirst extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        tvFrag1 = view.findViewById(R.id.tvFrag1);
+        btnAddText = view.findViewById(R.id.btnAddTextFrag1);
+
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = tvFrag1.getText().toString() + "\n" + "New Data";
+                tvFrag1.setText(data);
+            }
+        });
+
+        return view;
     }
+
 }
